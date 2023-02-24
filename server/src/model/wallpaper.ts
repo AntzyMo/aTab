85e68071e36e045a2407b6d9cde2513018076cdd
AntzyMo <mozbano@163.com>
2023-02-24 18:09:42 +0800

    chore: 不压缩图片

--- server/src/model/wallpaper.ts
+++ server/src/model/wallpaper.ts
@@ -1,12 +1,6 @@
 import { getItabBingWallpaper as getItabBingWallpaperApi } from '../api'
 
-const replacePngToWebp = (str: string) => str.replaceAll(/\png|jpg|gif|jpeg/g, 'webp')
-
 export const getItabBingWallpaper = async (page: string) => {
   const { data } = await getItabBingWallpaperApi(page)
-  data.forEach(item => {
-    item.fullSrc = replacePngToWebp(item.fullSrc)
-    item.thumb = replacePngToWebp(item.thumb)
-  })
   return data
 }
